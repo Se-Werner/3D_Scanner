5aa02bb8d230eb2869f1ac5a6cfca84bf4b9b4f3
Sebastian Werner <sebastian.werner@se-werner.de>
2025-07-05 22:11:40 +0200

    add Lidar Sensor and main program

--- component_classes.py
+++ component_classes.py
@@ -1,5 +1,6 @@
-from machine import Pin
+from machine import Pin, I2C
 import utime
+from vl53l0x import VL53L0X
 
 
 class StepperMotor:
@@ -20,6 +21,11 @@ class StepperMotor:
 
     def step(self, direction, steps, speed):
 
+        if self.max_position < self.current_position + (steps * direction):
+            steps = self.max_position - self.current_position
+        elif self.current_position + (steps * direction) < 0:
+            steps = self.current_position
+
         for i in range(steps):
             self.step_index = (self.step_index + direction) % len(self.step_sequence)
             self.current_position += direction
@@ -28,12 +34,39 @@ class StepperMotor:
                 self.pin_out[pin_index].value(pin_value)
             utime.sleep(speed)
 
-    def set_current_position(self, position):
-        self.current_position = position
+    def set_current_position(self):
+        self.current_position = 0
 
-    def set_max_position(self, position):
-        self.max_position = position
+    def set_max_position(self):
+        self.max_position = self.current_position
 
     def release_stepper(self):
         for pin in self.pin_out:
             pin.value(0)
+
+    def move_to_position(self, position, speed):
+
+        target_position = self.max_position * (position / 100)
+
+        steps_needed = target_position - self.current_position
+        if steps_needed < 0:
+            direction = -1
+        elif steps_needed > 0:
+            direction = 1
+
+        self.step(direction, steps_needed, speed)
+
+
+class LidarSensor:
+    def __init__(self, id, sda_pin, scl_pin):
+
+        self.i2c = I2C(id=id, sda=Pin(sda_pin), scl=Pin(scl_pin))
+
+        self.sensor = VL53L0X(self.i2c)
+        self.sensor.set_measurement_timing_budget(40_000)
+        self.sensor.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 12)
+        self.sensor.tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 8)
+
+    def read(self):
+        measurment_value = self.sensor.ping()
+        return measurment_value
